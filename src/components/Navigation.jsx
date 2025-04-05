98f6700cf91a8b348fad0f1469f43558cd01d887
Navid <navidmulla60@gmail.com>
2025-04-05 16:16:13 +0530

    added prpic

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -13,19 +13,19 @@ const Navigation = () => (
     <Button component={Link} to="/" sx={{ mx: 1 }}>
       Home
     </Button>
-    <Button href="#projects" sx={{ mx: 1 }}>
+    <Button href="projects" sx={{ mx: 1 }}>
       Projects
     </Button>
-    <Button href="#experience" sx={{ mx: 1 }}>
+    <Button href="experience" sx={{ mx: 1 }}>
       Experience
     </Button>
-    <Button href="#skills" sx={{ mx: 1 }}>
+    <Button href="skills" sx={{ mx: 1 }}>
       Skills
     </Button>
-    <Button href="#publications" sx={{ mx: 1 }}>
+    <Button href="publications" sx={{ mx: 1 }}>
       Publications
     </Button>
-    <Button href="#contact" sx={{ mx: 1 }}>
+    <Button href="contact" sx={{ mx: 1 }}>
       Contact
     </Button>
   </Box>
